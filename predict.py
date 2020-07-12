ade32c2f481995b77aa9e911cbe46dc8f9836308
proloving <2667737165@qq.com>
2020-07-12 18:02:39 +0800

    first commit

---
+++ predict.py
@@ -0,0 +1,53 @@
+import os
+import json
+import math
+import matplotlib.pyplot as plt
+from core.data_processor import DataLoader
+from core.model import Model
+
+def plot_results(predicted_data, true_data):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+    plt.plot(predicted_data, label='Prediction')
+    plt.legend()
+    plt.show()
+
+
+def plot_results_multiple(predicted_data, true_data, prediction_len):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+	# Pad the list of predictions to shift it in the graph to it's correct start
+    for i, data in enumerate(predicted_data):
+        padding = [None for p in range(i * prediction_len)]
+        plt.plot(padding + data, label='Prediction')
+        plt.legend()
+    plt.show()
+
+def main():
+    configs = json.load(open('config.json', 'r'))
+    model = Model()
+    model.load_model("./saved_models/model2.h5")
+
+    data = DataLoader(
+        os.path.join('data', configs['data']['filename']),
+        configs['data']['train_test_split'],
+        configs['data']['columns']
+    )
+
+    x_test, y_test = data.get_test_data(
+        seq_len=configs['data']['sequence_length'],
+        normalise=configs['data']['normalise']
+    )
+
+    # predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'], configs['data']['sequence_length'])
+    # predictions = model.predict_sequence_full(x_test, configs['data']['sequence_length'])
+    predictions = model.predict_point_by_point(x_test)
+
+    # plot_results_multiple(predictions, y_test, configs['data']['sequence_length'])
+    plot_results(predictions, y_test)
+
+
+if __name__ == '__main__':
+    main()
